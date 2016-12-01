e49e24b748274da5ae556001750099c52ba2749e
Thomas Maxwell Smith <faotms@gmail.com>
2016-12-01 16:25:19 +0000

    2nd wireframe sort of done

--- source/js/include/nav.js
+++ source/js/include/nav.js
@@ -1,4 +1,4 @@
-$('.products').on('click', function(event) {
+$('.product-btn').on('click', function(event) {
 	event.preventDefault();
 	if (!$(this).hasClass('active')) {
 		$(this).addClass('active');
@@ -6,6 +6,32 @@ $('.products').on('click', function(event) {
 	} else {
 		$(this).removeClass('active');
 		$('.dropdown').hide().removeClass('fadeInDown');
+	}
+	
+});
+
+$('.product-btn2').on('click', function(event) {
+	event.preventDefault();
+	var id = $(this).data('drop');
+	var $dropdown = $('[data-down="' + id + '"]');
+	console.log(id, $dropdown);
+	if (!$(this).hasClass('active')) {
+		$(this).addClass('active');
+		$dropdown.show().addClass('fadeInDown');
+	} else {
+		$(this).removeClass('active');
+		$dropdown.hide().removeClass('fadeInDown');
+	}
+});
+
+$('.bang-btn').on('click', function(event) {
+	event.preventDefault();
+	if (!$(this).hasClass('active')) {
+		$(this).addClass('active');
+		$('.dropdown3').show().addClass('fadeInDown');
+	} else {
+		$(this).removeClass('active');
+		$('.dropdown3').hide().removeClass('slideInLeft');
 	}
 	
 });
