f2cbbb2589b804c782d6bdeda0755a50980f6ba6
Thomas Maxwell Smith <faotms@gmail.com>
2017-01-16 14:24:13 +0000

    grabs results from categories

--- source/js/include/filter-mobile.js
+++ source/js/include/filter-mobile.js
@@ -32,6 +32,7 @@ var MobileFilter = function() {
 			parentName: null,
 			parentDom: null
 		},
+		filteredData: null,
 	};
 
 	var domCache = {
@@ -51,6 +52,7 @@ var MobileFilter = function() {
 				state.setActiveCategory($this);
 				_setSubheadings();
 				_switchRadioButton($this);
+				_getProductData(state.currentCategory.name);
 			}
 			
 		});
@@ -72,6 +74,75 @@ var MobileFilter = function() {
 		domCache.currentSelectionMaster.html(textTemplate);
 		$('.current-selection--parent').html('');
 		state.currentCategory.parentDom.find('.current-selection--parent').html(state.currentCategory.name);
+
+	}
+
+	function _getProductData(categoryName) {
+		// get data
+		var cachedData = data.navigation;
+		var results = data.navigation;
+		var products = [];
+		// select home cinema
+		if (!categoryName) {
+
+			for (var i = 0; i < results.length; i++) {
+
+				for (var x = 0; x < results[i]['child-category'].length; x++) {
+
+					for (var y = 0; y < results[i]['child-category'][x].products.length; y++) {
+						var name = results[i]['child-category'][x].products[y].title;
+						var price = results[i]['child-category'][x].products[y].nid;
+						var strapline = results[i]['child-category'][x].products[y].strapline;
+						var colours = results[i]['child-category'][x].products[y].colours;
+						products.push({name, price, strapline, colours});
+						//products.push(productMarkup(name, price, strapline, colours));
+					}
+				}
+			}
+		} else {
+			// also look at categories
+			for (var i = 0; i < cachedData.length; i++) {
+
+				for (var x = 0; x < cachedData[i]['child-category'].length; x++) {
+
+
+					if (cachedData[i]['child-category'][x].title === categoryName) {
+						
+						for (var y = 0; y < cachedData[i]['child-category'][x].products.length; y++) {
+							var name = cachedData[i]['child-category'][x].products[y].title;
+							var price = cachedData[i]['child-category'][x].products[y].nid;
+							var strapline = cachedData[i]['child-category'][x].products[y].strapline;
+							var colours = cachedData[i]['child-category'][x].products[y].colours;
+							products.push(productMarkup(name, price, strapline, colours));
+							
+						}
+					}
+				}
+			}
+		}
+		
+		// var $list = $('.results-list');
+		// if (products.length) {
+		// 	$list.html('');
+		// 	for (var i = 0; i < products.length; i++) {
+		// 		$list.append(products[i])
+		// 	}
+		// 	$('.found-count--number').text($('.results-list .result---product').length);
+
+		// } else {
+		// 	$list.html('<div>' +  'No results' + '</div>');
+		// 	$('.found-count--number').text('0');
+		// }
+		console.log(products);
+		$('.results-list').html(products);
+		$('.found-count--number').text(products.length);
+		slinky.home();
+		setTimeout(function() {
+			$('html, body').animate({
+		        scrollTop: $(".mob--container").offset().top
+		    }, 300);
+		}, 200);
+		
 	}
 
 	return {
